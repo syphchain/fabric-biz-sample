7873c3900b665750f8d76c1fea6714d28edda5a9
shniu <niushaohan@digcredit.com>
2018-12-13 18:02:22 +0800

    add start server background

---
+++ billEndorse/startServer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+function installNodeModules() {
+	echo
+	if [ -d node_modules ]; then
+		echo "============== node modules installed already ============="
+	else
+		echo "============== Installing node modules ============="
+		npm install
+	fi
+	echo
+}
+
+
+if [ -d logs ]; then
+	echo "============== logs already existed ============="
+else
+	echo "============== mkdir logs ============="
+	mkdir logs
+fi
+
+# start node
+forever start -l logs/forever.log -o logs/out.log -e logs/err.log app.js
